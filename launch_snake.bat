eb82837aff71a63459707407034be0e1f8974ce6
aliae <noam.carmi80@gmail.com>
2025-10-27 00:02:56 +0100

    place net on essaye de reprendre a 0

--- launch_snake.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo 🐍 Lancement du Snake Game...
-echo.
-build\SnakeGame.exe
-echo.
-echo Jeu terminé! 
-pause
--- start_snake.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Lancement du Snake Game...
-echo.
-cd /d "%~dp0"
-build\SnakeGame.exe
-echo.
-echo Appuyez sur une touche pour fermer...
-pause >nul
